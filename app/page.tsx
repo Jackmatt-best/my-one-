743b9fd343707fa081dd738f691645e29f1036a0
Jackmatt-best <matttechs4@gmail.com>
2025-04-26 13:27:04 +0300

    Adding components to the main page file (page.tsx)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import { Contact } from "lucide-react";
+import AboutSection from "./components/AboutSection";
+import Header from "./components/Header";
+import HeroSection from "./components/Herosection";
+import ProjectsSection from "./components/ProjectSection";
+import SkillsSection from "./components/SkillsSection";
+
 export default function Page() {
   return (
-    <div>
-      <h1 className="underline">Welcome</h1>
-    </div>
+    <>
+        <div>
+          <Header />
+          <main>
+            <HeroSection />
+            <AboutSection />
+            <SkillsSection />
+            <ProjectsSection />
+            <Contact />
+          </main>
+        </div>
+    </>
   )
 }
